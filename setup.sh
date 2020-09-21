3729ce63184e225ce89d9ab9d81442e23d1b6b08
ilyaasp <ilyas.hammam17@gmail.com>
2020-09-21 23:40:54 +0700

    test

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 mkdir -p ~/.streamlit/
+
 echo "\
 [general]\n\
 email = \"ilyas.hammam17@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
+
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
-
+" > ~/.streamlit/config.toml
